91a152c45750283cd12dc7f555aa2c5054cbaea6
Jim Van Fleet <jim@jimvanfleet.com>
2020-04-18 16:57:31 -0400

    Just rake

--- Formula/lvl.rb
+++ Formula/lvl.rb
@@ -9,7 +9,7 @@ class Lvl < Formula
   def install
     system "rake"
     #system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    #bin.install_symlink Dir["#{libexec}/bin/*"]
   end
   
 end
