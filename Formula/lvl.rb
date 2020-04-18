ab2b973ce5dfbfcc0fd8d2fce05b5ef16684182f
Jim Van Fleet <jim@jimvanfleet.com>
2020-04-18 16:17:40 -0400

    Adjusting filename

--- Formula/lvl.rb
+++ Formula/lvl.rb
@@ -1,7 +1,7 @@
 class Lvl < Formula
   desc "lvl CLI: the official Levvel CLI"
   homepage "https://levvel.io/"
-  url "http://127.0.0.1:32768/foo-0.1.tar.gz"
+  url "http://127.0.0.1:32768/lvl-0.1.tar.gz"
 
   
   depends_on "node"
