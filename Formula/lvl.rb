7e912bdddc6d8b06a97fe2942fd6f86a6e1628f0
Jim Van Fleet <jim@jimvanfleet.com>
2020-04-18 16:09:31 -0400

    Monkeying around to see .tar.gz reqs

--- Formula/lvl.rb
+++ Formula/lvl.rb
@@ -1,8 +1,9 @@
 class Lvl < Formula
-    desc "lvl CLI: the official Levvel CLI"
-    homepage "https://levvel.io/"
-    url "https://example.com/foo-0.1.tar.gz"
+  desc "lvl CLI: the official Levvel CLI"
+  homepage "https://levvel.io/"
+  url "http://127.0.0.1:32768/foo-0.1.tar.gz"
 
   
-  depends_on "nodenv"
+  depends_on "node"
+  
 end
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# homebrew-lvl
+# `lvl`: The Official CLI of Levvel
+
+## How do I install these formulae?
+`brew tap bigfleet/lvl` and then `brew install lvl`.
+
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
